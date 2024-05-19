a199b662ae32fd42b210ae87d2a4899d2262ed60
QuangTran <quangtrancnh@gmail.com>
2024-05-19 17:37:24 +0700

    fix bug and add connection detail page

--- src/components/page-header/PageHeader.tsx
+++ src/components/page-header/PageHeader.tsx
@@ -1,16 +1,28 @@
 import { FaArrowLeft } from 'react-icons/fa';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Col, Row } from 'antd';
 
 interface PageHeaderProps {
   title: string;
+  button?: any;
 }
-export const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+export const PageHeader: React.FC<PageHeaderProps> = ({ title, button }) => {
   const navigate = useNavigate();
   return (
-    <div className="px-5 py-2 flex items-center gap-2 border-b-1 border-solid border-border">
-      <FaArrowLeft className="text-primary hover:cursor-pointer" onClick={() => navigate(-1)} />
-      <span className="text-xl font-bold">{title}</span>
-    </div>
+    <Row className="px-5 py-2 flex flex-row items-center gap-2 border-b-1 border-solid border-border">
+      <Col span={5}>
+        <FaArrowLeft
+          className="text-primary hover:cursor-pointer mr-2"
+          onClick={() => navigate(-1)}
+        />
+        <span className="text-xl font-bold">{title}</span>
+      </Col>
+      {button && (
+        <Col span={10} className="flex gap-2 justify-start">
+          {button}
+        </Col>
+      )}
+    </Row>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 import { AppLayout } from '../layout/app/AppLayout.tsx';
 import { MigrationJobs } from './migration-jobs/MigrationJobs.tsx';
-import { ConnectionProfiles } from './connection-profiles/ConnectionProfiles.tsx';
+import { ConnectionProfiles } from './connection-profiles';
 import { PrivateConnectivity } from './private-connectivity/PrivateConnectivity.tsx';
 import { ConversionWorkspaces } from './conversion-workspaces/ConversionWorkspaces.tsx';
 import { Error404 } from '../components/errors/Error404.tsx';
@@ -10,6 +10,7 @@ import { Error403 } from '../components/errors/Error403.tsx';
 import { CreateMigrationJobs } from './migration-jobs/create-migration-jobs';
 import { JobDetail } from './job-detail';
 import { TaskDetail } from './job-detail/component/TaskDetail.tsx';
+import { ConnectionDetails } from './connection-profiles/ConnectionDetails.tsx';
 
 export const AppRoutes: React.FC = () => {
   return (
@@ -43,6 +44,8 @@ export const AppRoutes: React.FC = () => {
         <Route path="/migration-jobs/create" element={<CreateMigrationJobs />} />
 
         <Route path="/connection-profiles" element={<ConnectionProfiles />} />
+        <Route path="/connection-profiles/:id" element={<ConnectionDetails />} />
+
         <Route path="/private-connectivity" element={<PrivateConnectivity />} />
         <Route path="/conversion-workspaces" element={<ConversionWorkspaces />} />
       </Route>
